2a948fe1aa1019c62bc170c7fa3041afa0930fde
Jinsoo <81242485+jumosd@users.noreply.github.com>
2022-06-30 17:59:12 +0900

    네트워크 추출 ㅠㅠ 감격

--- craw.py
+++ craw.py
@@ -10,7 +10,7 @@ wb = openpyxl.Workbook()
 # 3. 작업할 Workbook 내 Sheet 활성화 
 sheet = wb.active
 # 4. Sheet 내 Cell 선택 (A1셀에 1이라는 값 할당) 
-sheet.append(["순위","게임이름", "장르", "네트웤"])
+sheet.append(["순위","게임이름", "장르", "네트워크"])
 
 url = 'https://playtoearn.net/blockchaingames/All-Blockchain/All-Genre/Live/All-Device/All-NFT/All-PlayToEarn/All-FreeToPlay'
 
@@ -32,38 +32,50 @@ p2egame_list = {
     'network' :[],
 
 }
-rank_num = 0
+rank_num = -1
 
 for element in elements:
     
     if element is not None:
 
         name = element.select_one('tbody > tr > td > a > div.dapp_name > span:nth-child(1)').string
-        genres = element.select('td:nth-child(4) > a')
-        genre_num = len(genres)
-        print("------------------------------------------------------------------------------------------")
-        print(len(genres))
-        
-
         p2egame_list['name'].append(name)
 
+
+        
+        
+        # <a data-original-title="Wemix" data-placement="top" data-toggle="tooltip" href="https://playtoearn.net/blockchaingames/Wemix/All-Genre/All-Status/All-Device/All-NFT/All-PlayToEarn/All-FreeToPlay" title="">
+        #     <div class="lazy platformicon Wemix loaded" data-loader="bgLoader"></div>
+        # </a>
+
         genre_list =[]
+        network_list=[]
+
+        genres = element.select('td:nth-child(4) > a')
+        genre_num = len(genres)
         for i in genres:
             genre = str(i.string)
             genre_list.append(genre)
 
-        
+        networks = element.select('td:nth-child(5) > a')
+        for i in networks:
+            network = i.attrs['data-original-title']
+            network_list.append(network)
+
+
         genre_list_str = ','.join(genre_list)
         genre_list_str = str(genre_list_str)
+
+        network_list_str = ','.join(network_list)
+        network_list_str = str(network_list_str)
         
         p2egame_list['genre'].append(genre_list_str)
+        p2egame_list['network'].append(network_list_str)
         
-        
-    
         rank_num += 1    
-        sheet.append([rank_num,name,genre_list_str, ''])
+        sheet.append([rank_num,name,genre_list_str,network_list_str])
 
-        print(f"순위: {rank_num}    게임이름: {name}    장르: {genre_list_str} ")
+        print(f"순위: {rank_num}    게임이름: {name}    장르: {genre_list_str}    네트워크: {network_list_str}")
     
     
     else:
