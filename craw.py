146362452cc65a1c50f68091de41cdcc642c3251
Hajinsoo <jumosd@icloud.com>
2022-07-08 15:37:52 +0900

    9페이지 돌아가면서 가져오기

--- craw.py
+++ craw.py
@@ -19,142 +19,149 @@ sheet = wb.active
 # 4. Sheet 내 Cell 선택 (A1셀에 1이라는 값 할당) 
 sheet.append(["순위","게임이름", "장르", "네트워크", "디바이스", "NFT사용유무", "무료플레이", "얻을수있는것", "p2e사이트 상세페이지 링크"])
 
-url = 'https://playtoearn.net/blockchaingames/All~~~-Blockchain/All-Genre/Live/All-Device/All-NFT/All-PlayToEarn/All-FreeToPlay'
-
-
-driver=webdriver.Chrome('./chromedriver 5') 
-driver.get(url)
-#  로딩이 끝날때 까지 기다려주세요 
-driver.implicitly_wait(1000)
-
-html = driver.page_source
-
-soup = BeautifulSoup(html, 'html.parser')
-
-
-
-elements = soup.select('body > div.container > div.indexmaintable.indexpage > div > table.table.table-bordered.mainlist > tbody > tr')
-
-
-
-p2egame_list = {
-    'name' :[],
-    'genre' :[],
-    'network' :[],
-    'device' :[],
-    'NFT' :[],
-    'F2P' :[],
-    'P2E' :[],
-    
-
-}
+cur_page = 1
 rank_num = 0
 
-for idx , element in enumerate(elements):
+while cur_page <= 9:
+    url = f"https://playtoearn.net/blockchaingames/All-Blockchain/All-Genre/Live/All-Device/All-NFT/All-PlayToEarn/All-FreeToPlay?sort=socialscore_24h&amp%3Bdirection=desc&amp%3Bpage=%7Bcur_page2&direction=desc&page={str(cur_page)}"
     
-    if element is not None:
+    driver=webdriver.Chrome('./chromedriver 5') 
+    driver.get(url)
+    #  로딩이 끝날때 까지 기다려주세요 
+    driver.implicitly_wait(1000)
 
-        name = element.select_one('tbody > tr > td > a > div.dapp_name > span:nth-child(1)').string
-        p2egame_list['name'].append(name)
+    html = driver.page_source
 
-        genre_list = []
-        network_list= []
-        device_list= []
-        F2P_list= []
-        P2E_list= []
-        
-        
-        genres = element.select('td:nth-child(4) > a')
-        for i in genres:
-            genre = str(i.string)
-            genre_list.append(genre)
-
-        networks = element.select('td:nth-child(5) > a')
-        for i in networks:
-            network = i.attrs['data-original-title']
-            network_list.append(network)
-
-        devices = element.select('td:nth-child(6) > a')
-        for i in devices:
-            device = i.attrs['data-original-title']
-            device_list.append(device)
-
-        nft = element.select_one('td:nth-child(8) > a').string
-        p2egame_list['NFT'].append(nft)
-
-        f2p = element.select_one('td:nth-child(9) > a').string
-        p2egame_list['NFT'].append(f2p)
-
-        p2es = element.select('td:nth-child(10) > a')
-        for i in p2es:
-            p2e = i.string
-            P2E_list.append(p2e)
+    soup = BeautifulSoup(html, 'html.parser')
 
 
-        genre_list_str = ','.join(genre_list)
-        genre_list_str = str(genre_list_str)
 
-        network_list_str = ','.join(network_list)
-        network_list_str = str(network_list_str)
+    elements = soup.select('body > div.container > div.indexmaintable.indexpage > div > table.table.table-bordered.mainlist > tbody > tr')
 
-        device_list_str = ','.join(device_list)
-        device_list_str = str(device_list_str)
 
-        P2E_list_str = ','.join(P2E_list)
-        P2E_list_str = str(P2E_list_str)
 
-        p2egame_list['genre'].append(genre_list_str)
-        p2egame_list['network'].append(network_list_str)
-        p2egame_list['device'].append(device_list_str)
-        p2egame_list['P2E'].append(P2E_list_str)
-        
+    p2egame_list = {
+        'name' :[],
+        'genre' :[],
+        'network' :[],
+        'device' :[],
+        'NFT' :[],
+        'F2P' :[],
+        'P2E' :[],
         
 
-        driver.quit()
-        if idx != 0:
-            rank_num += 1
-            
-           
+    }
+    
 
-            try:
-                detail_url =  element.find('a', {'class':'dapp_detaillink'}).attrs['href']
-                sheet.append([rank_num,name,genre_list_str,network_list_str,device_list_str,nft,f2p,P2E_list_str,detail_url])
-                print(f"순위: {rank_num}    게임이름: {name}  장르: {genre_list_str}  네트워크: {network_list_str}  디바이스: {device_list_str}  NFT: {nft}  F2P: {f2p} P2E: {P2E_list_str} 상세링크: {detail_url}")
-                print (detail_url, type(detail_url))
-                
-                
-                Folder_name = (str(idx) + "_" + str(p2egame_list['name'][rank_num]))
-                # if 페이지를 가져오면 밑에꺼출력
-                os.mkdir('./Result/'+Folder_name)
-                
-                # # 이부분 이 이상
-                # req_get_txt = requests.get(detail_url).text
-                # print(req_get_txt)
-                # driver.get(detail_url)
-                # driver.implicitly_wait(10)
+    for idx , element in enumerate(elements):
+        
+        if element is not None:
 
-                # try:
-                #     element = WebDriverWait(driver, 10).until(
-                #         EC.presence_of_element_located((By.CLASS_NAME, "owl-item"))
-                #     )
-                # finally:
-                #     driver.quit()
-                
+            name = element.select_one('tbody > tr > td > a > div.dapp_name > span:nth-child(1)').string
+            p2egame_list['name'].append(name)
 
-                # else 실패하면
-                #os.mkdir('./Result/' +str(a) + "_" + "사진X_" +str(p2egame_list['name'][a]))
-                continue
-
-            except OSError:
-                # if e.errno != errno.EEXIST:
-                #     raise   
-                # time.sleep might help here
-                pass
+            genre_list = []
+            network_list= []
+            device_list= []
+            F2P_list= []
+            P2E_list= []
             
             
-        None
+            genres = element.select('td:nth-child(4) > a')
+            for i in genres:
+                genre = str(i.string)
+                genre_list.append(genre)
 
+            networks = element.select('td:nth-child(5) > a')
+            for i in networks:
+                network = i.attrs['data-original-title']
+                network_list.append(network)
+
+            devices = element.select('td:nth-child(6) > a')
+            for i in devices:
+                device = i.attrs['data-original-title']
+                device_list.append(device)
+
+            nft = element.select_one('td:nth-child(8) > a').string
+            p2egame_list['NFT'].append(nft)
+
+            f2p = element.select_one('td:nth-child(9) > a').string
+            p2egame_list['NFT'].append(f2p)
+
+            p2es = element.select('td:nth-child(10) > a')
+            for i in p2es:
+                p2e = i.string
+                P2E_list.append(p2e)
+
+
+            genre_list_str = ','.join(genre_list)
+            genre_list_str = str(genre_list_str)
+
+            network_list_str = ','.join(network_list)
+            network_list_str = str(network_list_str)
+
+            device_list_str = ','.join(device_list)
+            device_list_str = str(device_list_str)
+
+            P2E_list_str = ','.join(P2E_list)
+            P2E_list_str = str(P2E_list_str)
+
+            p2egame_list['genre'].append(genre_list_str)
+            p2egame_list['network'].append(network_list_str)
+            p2egame_list['device'].append(device_list_str)
+            p2egame_list['P2E'].append(P2E_list_str)
+            
+            detail_url =  element.find('a', {'class':'dapp_detaillink'}).attrs['href']
+
+            driver.quit()
+            if idx != 0:
+                rank_num += 1
+                
+            
+
+                try:
+                    
+                    
+                    print(f"순위: {rank_num}    게임이름: {name}  장르: {genre_list_str}  네트워크: {network_list_str}  디바이스: {device_list_str}  NFT: {nft}  F2P: {f2p} P2E: {P2E_list_str} 상세링크: {detail_url}")
+                    sheet.append([rank_num,name,genre_list_str,network_list_str,device_list_str,nft,f2p,P2E_list_str,detail_url])
+                    
+                    
+                    Folder_name = (str(rank_num) + "_" + str(p2egame_list['name'][idx]))
+                    # if 페이지를 가져오면 밑에꺼출력
+                    os.mkdir('./Result/'+Folder_name)
+                    
+                    # # 이부분 이 이상
+                    # req_get_txt = requests.get(detail_url).text
+                    # print(req_get_txt)
+                    # driver.get(detail_url)
+                    # driver.implicitly_wait(10)
+
+                    # try:
+                    #     element = WebDriverWait(driver, 10).until(
+                    #         EC.presence_of_element_located((By.CLASS_NAME, "owl-item"))
+                    #     )
+                    # finally:
+                    #     driver.quit()
+                    
+
+                    # else 실패하면
+                    #os.mkdir('./Result/' +str(a) + "_" + "사진X_" +str(p2egame_list['name'][a]))
+                    continue
+
+                except OSError:
+                    # if e.errno != errno.EEXIST:
+                    #     raise
+                    # time.sleep might help here
+                    pass
+                
+               
+            None
+            
+    cur_page += 1 
 wb.save("excel_1.xlsx")
+
+
+
 
 # # 게임이름 URL에 넣기위해 변형한다
 # detail_pages_url =[]
